e04d2a80e3e19f31bf4df66520820c61f6378504
Fedotov Aleksey <far@rio-soft.ru>
2021-01-13 10:30:41 +0500

    Несколько компонентов и заготовки для страниц

---
+++ pages/portfolios/[id].tsx
@@ -0,0 +1,25 @@
+import {useRouter} from 'next/router';
+import {NextPage} from 'next';
+import {Navbar} from '../../components/shared/NavBar';
+
+interface IProps {
+    id: string | string[];
+}
+
+const PortfolioDetail: NextPage<IProps> = ({id}) => {
+	return (
+		<>
+			<Navbar />
+			<div className="container">
+				<h1>Detail page for {id} </h1>
+			</div>
+		</>
+	);
+};
+
+PortfolioDetail.getInitialProps = ({query}) => {
+    const id = query.id;
+    return {id};
+}
+
+export default PortfolioDetail;
